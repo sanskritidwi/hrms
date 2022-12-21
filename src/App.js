d9f3722d6d74e50c1f8f71431a91fbcf550ae7aa
Varmanisha <nisha.varma@cubexo.io>
2022-12-21 18:26:51 +0000

    popup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
+import React from 'react';
+import Button from 'react-bootstrap/Button';
+import { Modalpopup } from './common/Popup';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.min.js';
 import './App.css';
-
+import './styles/css/all.css'
+// import './styles/sass/all.scss'
 function App() {
+  const [modalShow, setModalShow] = React.useState(false);
   return (
     <div className="App">
-    HRM Cubexo
+      {/* modal-start */}
+      <Button variant="primary" onClick={() => setModalShow(true)}>
+        Mark Now
+      </Button>
+
+      <Modalpopup
+        show={modalShow}
+        onHide={() => setModalShow(false)}
+        title='Add Event'
+      />
     </div>
   );
 }
