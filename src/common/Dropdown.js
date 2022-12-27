66225376fd5aba09999b86d7b9781d72c971b497
kavita.mishra@cubexo.io <kavita.mishra@cubexo.io>
2022-12-27 11:40:57 +0530

    wip Navbar

---
+++ src/common/Dropdown.js
@@ -0,0 +1,22 @@
+
+const Dropdown = () => {
+    
+    const renderDropdown = () => {
+        return(
+            <>
+                <div className="Dropdown-wrapper">
+
+                </div>
+            </>
+        )
+    }
+    
+    return(
+        <>
+            <button>Notifications</button>
+            
+        </>
+    )
+}
+
+export default Dropdown;
