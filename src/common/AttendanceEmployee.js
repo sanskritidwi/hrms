f0a463036e2351e68eec8e3164bf2447968b7833
kavita.mishra@cubexo.io <kavita.mishra@cubexo.io>
2023-01-04 11:18:14 +0530

    incomplete attendance component

--- src/common/AttendanceEmployee.js
+++ src/common/AttendanceEmployee.js
@@ -1,27 +1,60 @@
-import React from "react"
+import React, { useState } from "react"
 import { PageHeader } from "./PageHeader"
 import ProgressBar from "./progressBar";
-export const AttendanceEmployee = () => {
+function padTo2Digits(num) {
+    return num.toString().padStart(2, '0');
+  }
+  
+  function convertMsToHM(milliseconds) {
+    let seconds = Math.floor(milliseconds / 1000);
+    let minutes = Math.floor(seconds / 60);
+    let hours = Math.floor(minutes / 60);
+  
+    seconds = seconds % 60;
+    // 👇️ if seconds are greater than 30, round minutes up (optional)
+    minutes = seconds >= 30 ? minutes + 1 : minutes;
+  
+    minutes = minutes % 60;
+  
+    // 👇️ If you don't want to roll hours over, e.g. 24 to 00
+    // 👇️ comment (or remove) the line below
+    // commenting next line gets you `24:00:00` instead of `00:00:00`
+    // or `36:15:31` instead of `12:15:31`, etc.
+    hours = hours % 24;
+  
+    return `${padTo2Digits(hours)}:${padTo2Digits(minutes)}:${padTo2Digits(seconds)}`;
+  }
+    const milliseconds = (h, m, s) => ((h*60*60+m*60+s)*1000);
+    export const AttendanceEmployee = ({percentage}) => {
+    let dt;
+    let dt2;
+    const handelTimeIn = () =>{
+        dt = new Date().getTime();
+        console.log(dt)
+     }
+     const handelTimeOut = () =>{
+         dt2 = new Date().getTime();
+         console.log(dt2)
+     }
+     const TotalTime = () =>{
+        // let total =Math.round((dt2-dt) / 1000);
+        var total = dt2 - dt
+        let timeData = convertMsToHM(total)
+        console.log(timeData)
+        console.log(total)
+        console.log( milliseconds(timeData), "dgjif")
+     }
     const renderInput = (timeing) => {
         var timeStatus = timeing;
         return (
             <>
                 <div className="d-flex align-items-center col-30">
                     <span>time {timeStatus} : </span>
-                    <button className="btn btn-outline-dark text-capitalize">mark {timeStatus}</button>
+                    {/* <button  className="btn btn-outline-dark text-capitalize" onClick={timeStatus==="In"? handelTimeIn: handelTimeOut}>mark {timeStatus}</button> */}
                 </div>
             </>
         )
     }
-    function diff_hours(dt2, dt1) 
-    {
-    var diff =(dt2.getTime() - dt1.getTime()) / 1000;
-    diff /= (60 * 60);
-    return Math.abs(Math.round(diff));
-    }
-    var dt1 = new Date();
-    var dt2 = new Date("January 2, 2023 11:13:00");
-    let percent = (diff_hours(dt1, dt2)/9)*100;
     return (
         <>
             <div className="AttendanceEmployee">
@@ -30,8 +63,11 @@ export const AttendanceEmployee = () => {
                     <button className="btn btn-dark text-capitalize dayStatus-btn">Working Day</button>
                 </div>
                 <div className="d-flex align-items-center col-30 justify-content-between px-3">
-                    {renderInput("In")}
-                    {renderInput("Out")}
+                <button  className="btn btn-outline-dark text-capitalize" onClick={handelTimeIn}>mark in</button>
+                <button  className="btn btn-outline-dark text-capitalize" onClick={handelTimeOut}>mark out</button>
+                <button  className="btn btn-outline-dark text-capitalize" onClick={TotalTime}> calculator</button>
+                    {/* {renderInput("In")}
+                    {renderInput("Out")} */}
                     <div className="d-flex align-items-center col-30">
                         <span>break duration: </span>
                         <button className="btn btn-outline-dark text-capitalize">mark </button>
@@ -39,12 +75,13 @@ export const AttendanceEmployee = () => {
                     <div style={{maxWidth:"380px", width:"100%",}}>
                         <div className="d-flex align-items-center col-30 justify-content-between">
                             <span>Working hours</span>
-                            <span>{diff_hours(dt1, dt2)} hours</span>
+                            {/* <span>{diff_hours(dt1, dt2)} hours</span> */}
                         </div>
-                        <ProgressBar completed={percent}/>
-                    </div>
+                        <ProgressBar completed={percentage}  max={'09:00:00'} inTime="January 3, 2023 11:13:00" outTime="January 3, 2023 2:13:00"/>
+                    </div> 
                 </div>
             </div>
         </>
     )
 }
+// completed={percent}handelDate min={timeData}
--- src/common/progressBar.js
+++ src/common/progressBar.js
@@ -1,14 +1,53 @@
 import React from "react";
-const ProgressBar = (props) => {
-    const { bgcolor, completed } = props;
+const ProgressBar = ( {completed, min ,max,inTime, outTime, getTime}) => {
+  console.log(inTime)
+  // console.log(outTime)
+    // const {completed } = props;
     const fillerStyles = {
       height: '100%',
       width: `${completed}%`,
       backgroundColor: "#05454E",
       borderRadius:50,
     }
+    
 
-  
+    // const calculateHour = () => {
+    //     let date = new Date()
+    //     console.log(date.getTime())
+        
+    //     let dt2 = date.getTime("07:00:00")
+    //     console.log(dt2, "jj")
+    //     let diff = Math.round((dt2 - date.getTime()) / 1000)
+    //     diff /= (60 * 60);
+    //     return Math.abs(diff);
+    // }
+    // let percentage = (calculateHour(inTime)/9)*100;
+    // console.log(percentage, "per")
+
+    // const calculateHour = (dt2, dt1) => {
+    //   let t = new Date()
+    //   let dtl2 = t.dt2.getTime();
+    //   console.log(dtl2, 'jhgj')
+    //   let dtl1 = t.dt1.getTime();
+    //   console.log(dtl1, 'njh')
+
+    //   let diff =Math.round((dtl2 - dtl1) / 1000);
+    //   diff /= (60 * 60);
+    //   return Math.abs(diff);
+    // }
+    // let percentage = (calculateHour(inTime,outTime)/9)*100
+     
+    // function diff_hours(dt2, dt1) 
+    // {
+    // var diff =(dt2.getTime() - dt1.getTime()) / 1000;
+    // diff /= (60 * 60);
+    // return Math.abs(Math.round(diff));
+    // }
+    // var dt1 = new Date();
+    // var dt2 = new Date("January 2, 2023 11:13:00");
+    // let percent = (diff_hours(dt1, dt2)/9)*100;
+
+
     return (
       <div className="ProgressBar">
         <div style={fillerStyles}>  
